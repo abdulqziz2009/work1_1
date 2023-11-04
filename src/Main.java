ea7ac75c87525d5423b941fcc54a598147990d50
abdulqziz2009 <148250194+abdulqziz2009@users.noreply.github.com>
2023-11-04 14:31:35 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Объявляем переменную типа String
+        String multipleWordString = "Несколько слов в строке";
+
+        // Создаем константу "num" и задаем ей значение
+        final int num = 5;
+
+        // Создаем переменную "word" и задаем ей значение
+        String word = "Пример";
+
+        // Задаем значение первой переменной из конкатенации "num" и "word"
+        String concatenatedString = num + word;
+
+        // Выводим значения переменных в одну строку
+        System.out.println("multipleWordString: " + multipleWordString);
+        System.out.println("num: " + num);
+        System.out.println("word: " + word);
+        System.out.println("concatenatedString: " + concatenatedString);
+
+        // Добавляем условную конструкцию
+        if (num < 0) {
+            System.out.println("Вы сохранили отрицательное число");
+        } else if (num > 0) {
+            System.out.println("Вы сохранили положительное число");
+        } else {
+            System.out.println("Вы сохранили ноль");
+        }
+
+        // Запрос имени пользователя
+        System.out.print("Введите ваше имя: ");
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();
+        System.out.println("Привет, " + userName + "!");
     }
 }
